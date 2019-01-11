95c18a769ae906dc4f443cfeff1ca9fb8bd28d67
Fernando Grangeiro <fjesus.appsimples@gmail.com>
2019-01-11 15:49:53 -0200

    First Activity

--- app/src/main/java/com/example/outsmart/billingcomponent/MainActivity.kt
+++ app/src/main/java/com/example/outsmart/billingcomponent/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.outsmart.billingcomponent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class Teste : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_teste)
+        setContentView(R.layout.activity_main)
     }
 }
